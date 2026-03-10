9e4363f935c9f91228281c4ce45ffa31f4076c34
Eyal Michaelov <eyalmic91@gmail.com>
2026-03-11 00:09:15 +0200

    new structure to try support loading sheet

---
+++ sheetLoader.js
@@ -0,0 +1,26 @@
+﻿const SHEET_URL =
+    "https://docs.google.com/spreadsheets/d/1uKdDpa3K1OxaTgexyih7flEi1Ter8L7XrAEsdeakqfE/export?format=csv&gid=0"
+
+async function loadSheet(){
+
+    const res = await fetch(SHEET_URL)
+    const text = await res.text()
+
+    const rows = text.split("\n").map(r => r.split(","))
+
+    const headers = rows[0]
+
+    const data = rows.slice(1).map(row => {
+
+        let obj = {}
+
+        headers.forEach((h,i)=>{
+            obj[h.trim()] = row[i]
+        })
+
+        return obj
+
+    })
+
+    return data
+}
